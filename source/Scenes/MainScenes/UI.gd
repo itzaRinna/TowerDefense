f41991a9968d15e039639a3324f2fb6e8c20d710
itzaRinna <dutchismahdick@gmail.com>
2024-04-30 07:27:27 +0700

    building towers&turrets mechanics

--- source/Scenes/MainScenes/UI.gd
+++ source/Scenes/MainScenes/UI.gd
@@ -6,9 +6,16 @@ func set_tower_preview(tower_type, mouse_position):
 	drag_tower.modulate = Color("ad54ff3c")
 	
 	var control = Control.new()
-	control.add_child(drag_tower)
+	control.add_child(drag_tower, true)
 	control.rect_position = mouse_position
 	control.set_name("TowerPreview")
 	add_child(control)
 	move_child(get_node("TowerPreview"), 0)
 
+func update_tower_preview(new_position, color):
+	var tower_preview = get_node("TowerPreview")
+	var drag_tower = tower_preview.get_node("DragTower")
+
+	tower_preview.rect_position = new_position
+	if drag_tower.modulate != Color(color):
+		drag_tower.modulate = Color(color)
